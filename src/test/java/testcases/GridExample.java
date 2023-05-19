2f62c8f2420e00f825b1b7101bdd37777c152413
srimanja <srimanja@amazon.com>
2023-05-19 19:57:19 +0530

    first committ

---
+++ src/test/java/testcases/GridExample.java
@@ -0,0 +1,39 @@
+package testcases;
+
+	import java.net.MalformedURLException;
+	import java.net.URL;
+
+	import org.openqa.selenium.Platform;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.remote.DesiredCapabilities;
+	import org.openqa.selenium.remote.RemoteWebDriver;
+	import org.testng.annotations.Test;
+
+	import io.github.bonigarcia.wdm.WebDriverManager;
+
+	public class GridExample {
+		
+		
+		@Test
+		public void testgrid() throws MalformedURLException {
+			
+			//System.setProperty(null, null);
+			//WebDriverManager.chromedriver().setup();
+			
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			
+			capabilities.setPlatform(Platform.WINDOWS);
+			capabilities.setBrowserName("chrome");
+			
+			URL hubURL = new URL("http://localhost:4444/wd/hub");
+			
+			WebDriver driver = new RemoteWebDriver(hubURL,capabilities );
+			
+			driver.get("https://www.simplilearn.com/");
+			
+			driver.quit();
+			
+		}
+	}
+
+
